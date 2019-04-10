79f598c41b7e080abcef4bc27142123f82fb84bd
lq199509 <674107158@qq.com>
2019-04-10 22:09:56 +0800

    banner图之一半

---
+++ src/components/banner.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="swiper-container billboards">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide"
+        v-for="item in banners"
+        :key="item.bannerId">
+        <img :src="item.imgUrl" alt="">
+      </div>
+    </div>
+    <!-- 分页器 -->
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      swiper: null
+    }
+  },
+  props: {
+    banners: {
+      type: Array   
+    }
+  },
+  updated () {
+    if(!this.swiper){
+      this.swiper = new Swiper('.swiper-container',{
+          loop: true,//连续轮播
+          pagination: {
+              el: '.swiper-pagination',
+          }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.swiper-container {
+  width: 100%;
+  height: 210px;
+
+  img {
+    width: 100%;
+  }
+
+  .swiper-pagination-bullet {
+    background: #fff;
+    opacity: 1;
+  }
+   
+  .swiper-pagination-bullet-active {
+    background: red;
+  }
+}
+</style>
--- src/views/Film.vue
+++ src/views/Film.vue
@@ -1,5 +1,57 @@
 <template>
   <div>
-    <h1>主页</h1>
+    <banner :banners='bannerList'></banner>
   </div>
 </template>
+
+<script>
+import banner from '../components/banner';
+import axios from 'axios';
+
+export default {
+  data () {
+    return {
+      bannerList: []
+    }
+  },
+  components: {
+    banner,
+  },
+  methods: {
+    getBanner () {
+      axios.get('https://m.maizuo.com/gateway?type=2&cityId=440300&k=9463487',{
+        headers: {
+          'X-Client-Info': '{"a":"3000","ch":"1002","v":"1.0.0","e":"1554895699231928234249"}',
+          'X-Host': 'mall.cfg.common-banner'
+        }
+      }).then(res => {
+        console.log(res.data);
+        let data = res.data;
+        if (data.status == 0) {
+          this.bannerList = data.data
+        } else {
+          alert(data.msg);
+        }
+      })
+      // axios.get('https://m.maizuo.com/gateway?type=2&cityId=440300&k=3684551', {
+      //   headers: {
+      //     'X-Client-Info': '{"a":"3000","ch":"1002","v":"1.0.0","e":"15546520171868310774513"}',
+      //     'X-Host': 'mall.cfg.common-banner'
+      //   }
+      // }).then(res => {
+      //   console.log(res.data);
+      //   let data = res.data;
+      //   if (data.status === 0) {
+      //     this.bannerList = data.data;
+      //   } else {
+      //     alert(data.msg);
+      //   }
+      // })
+    }
+  },
+  created() {
+    this.getBanner();
+  }
+}
+</script>
+
